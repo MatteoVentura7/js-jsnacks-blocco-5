a721b9665c1a25e323ab93bbc233f4ce246a68b1
MatteoVentura <matteo.ventura18@gmail.com>
2024-12-17 12:29:00 +0100

    add : snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,11 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+
+const numeriIncrementati = numbers.map(numero => numero + 1);
+
+
+console.log("Array originale:", numbers);
+console.log("Array incrementato:", numeriIncrementati);
+
 // Risultato: [3, 9, 5, 8, 3, 88]
