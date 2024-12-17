0039256733a6f545a9155986584ed2870d787fbc
MatteoVentura <matteo.ventura18@gmail.com>
2024-12-17 12:48:48 +0100

    add : snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -20,4 +20,9 @@ const students = [
 ];
 
 // Recupera i dati dello studente con id 2
+
+const studente = students.filter(studente => studente.id === 2);
+
+console.log(studente)
+
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
