790f5c274fcd3b8e3f9c4223ab9721e8b4ffe246
MatteoVentura <matteo.ventura18@gmail.com>
2024-12-17 12:58:51 +0100

    fix : snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,5 +2,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 
-console.log(names);
+
+names.forEach(namePeople => {
+    console.log(namePeople);
+  });
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
